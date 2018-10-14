74fc886742e8da6fdeda502693db2311bbce9f11
yasina <614867000@qq.com>
2018-10-14 16:59:55 +0800

    add year_weekday_to_day

--- dateutil.py
+++ dateutil.py
@@ -1,4 +1,5 @@
 import calendar
+from datetime import datetime,timedelta
 
 # 返回某年某月第n个weekday是几号
 def month_weekday_to_day(year,month,weekday,n):
@@ -16,10 +17,23 @@ def month_weekday_to_day(year,month,weekday,n):
 
     return -1 if day == 0 else day
 
+
+# 返回某年第n个weekday是几月几号
 def year_weekday_to_day(year,weekday,n):
     c = calendar.Calendar()
-
+    weeks = c.yeardayscalendar(year)
     
+    firstweek = weeks[0][0][0]
+
+    firstday = weeks[0][1][weekday] if firstweek[weekday] == 0 else firstweek[weekday]
+    targetdate = datetime(year,1,firstday) + timedelta((n-1)*7)
+
+    y,m,d = targetdate.year,targetdate.month,targetdate.day
+    if y != year:
+        return -1,-1
+
+    return m,d
+
 
 
 if __name__=='__main__':
@@ -30,8 +44,10 @@ if __name__=='__main__':
     # print(calendar.weekheader(1))
     # print(calendar.leapdays)
 
+
     c = calendar.Calendar()
     print(c.yeardayscalendar(2016))
+    print(year_weekday_to_day(2018,6,7))
     # print(c.monthdatescalendar(2018,11))
     # print(c.monthdays2calendar(2018,10))
 
